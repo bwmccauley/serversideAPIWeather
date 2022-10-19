803b381a5a4b644c99bb3a8b041d786edf5aded3
Brandon McCauley <brandonmccauley@Brandons-MacBook-Pro.local>
2022-10-19 19:29:45 -0400

    added fixes

--- main.js
+++ main.js
@@ -6,35 +6,38 @@ function getInfo(){
 
 
 // create a forLoops for the weather on the days
-fetch("https://api.openweathermap.org/data/3.0/onecall?lat={44.31t}&lon={83.04}&exclude=&appid={f7170118c6f5c729fe08831daba425f2}")
+fetch("https://api.openweathermap.org/data/3.0/onecall?lat=44.31&lon=83.04&exclude=&appid=f7170118c6f5c729fe08831daba425f2")
 .then(response => response.json())
 .then(data=>{
-for(i=0;i<5;i++){+
-    document.getElementById("day" +(i+1)+"Min").innerHTML ="Min:" +Number(data.list[i].main.temp_min -200.53).toFixed(1)+"°";
+    console.log (data)
+for(var i = 0; i<5 ;i++){
+    console.log("day" +(i+1)+"Min")
+    document.getElementById("day" +(i+1)+"Min").innerHTML ="Min:" +Number(data.daily[i].temp.min -200.53).toFixed(1)+"°";
 }
-    for(i=0;i<5;i++){+
-    document.getElementById("day" +(i+1)+"Max").innerHTML ="Max:" +Number(data.list[i].main.temp_max -200.53).toFixed(1)+"°";
+    for(var j = 0; j<5; j++){
+    document.getElementById("day" +(j+1)+"Max").innerHTML ="Max:" +Number(data.daily[j].temp.max -200.53).toFixed(1)+"°";
     
 }
-for(i=0;i<5;i++){+
-    document.getElementById("day" +(i+1)+"Max").innerHTML ="Max:" +Number(data.list[i].main.temp_max -200.53).toFixed(1)+"°";
-    
+// for(i=0;i<5;i++){
+//     document.getElementById("day" +(i+1)+"Max").innerHTML ="Max:" +Number(data.list[i].main.temp_max -200.53).toFixed(1)+"°";
+
+// }
+for(var k = 0; k<5; k++){
+    document.getElementById("img" +(k+1)).src = `https://openweathermap.org/img/wn/${data.daily[k].weather[0].icon}@2x.png`;
 }
-for(i=0;i<5;i++){
-    document.getElementById("img" +(i=1)).src =" http://openweathermap.org/img/wn/" +data.list[i].weather[0].icon+".png";
-}
+
 })
 
-.catch(err => alert("Something Went Wrong"))
+.catch(err => console.log (err))
 }
 
-function DefaultScreen(){
-    document.getElemntById("cityInput").defaultValue ="Detroit"
+function defaultScreen(){
+    document.getElementById("cityInput").defaultValue ="Detroit"
     getInfo();
 }
 const d =new Date();
 const weekday =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
+defaultScreen()
 function ChekDay(day){
     if (day +d.getDay() > 6){
         returnday +d.getDay()-7;  
@@ -44,6 +47,6 @@ function ChekDay(day){
     }
 }
 
-for(i=0;i<5;i++){
-    document.getEElemntById("day"+(i+1)).innerHTML = weekday[CheckDay]
-}
+// for(i=0;i<5;i++){
+   // document.getEElemntById("day"+(i+1)).innerHTML = weekday[CheckDay]
+// }
